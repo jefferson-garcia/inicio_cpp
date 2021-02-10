8cac698bf6f824603b02958922a1df09eb1598d7
Jefferson Andres <67662717+jefferson-garcia@users.noreply.github.com>
2021-02-10 18:33:25 -0500

    Update ensayo_2.py

--- ensayo_2.py
+++ ensayo_2.py
@@ -5,3 +5,7 @@ def mayor(a, b):
         return a
     return b
 
+numero_1 = int(input("Digite un número: "))
+numero_2 = int(input("Digite otro número: "))
+
+print(f"El número mayor es {mayor(numero_1, numero_2)})
